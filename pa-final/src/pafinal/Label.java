1caf41c97aa208ff1a0f75fb2051b92364014e18
andre.lotta86 <andre.lotta86@4c4e6b51-19cc-038f-6026-35b58781e471>
2010-07-14 13:24:24 +0000

    robina HTML

---
+++ pa-final/src/pafinal/Label.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pafinal;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ *
+ * @author andrealottarini
+ */
+public class Label implements WebComponent{
+    public String Text;
+
+    @Override
+    public void PreRender(List<String> headtext) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void Render(PrintStream output) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+}
