3576069bfdc7e1bebb6efa2e5eeba6765820dd59
andre.lotta86 <andre.lotta86@4c4e6b51-19cc-038f-6026-35b58781e471>
2010-07-16 15:39:29 +0000

    libreria :)

---
+++ WebComponent/src/mylib/ComponentPair.java
@@ -0,0 +1,28 @@
+package mylib;
+
+
+import java.io.PrintStream;
+import java.util.List;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author andrealottarini
+ */
+public class ComponentPair implements WebComponent {
+    WebComponent first;
+    WebComponent second;
+
+    public void preRender(List<String> headtext) {
+
+    }
+
+    public void render(PrintStream output, List<String> headText) {
+        
+    }
+
+}
