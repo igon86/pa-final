a9b5cc1ef0a33e706f0bf3e743078e3dbc5a6adf
andre.lotta86 <andre.lotta86@4c4e6b51-19cc-038f-6026-35b58781e471>
2010-07-16 15:40:05 +0000

    manca il render nested e pulire il tutto

--- trunk/PaRecursiveTree/src/PaFacesGenerator.java
+++ trunk/PaRecursiveTree/src/PaFacesGenerator.java
@@ -12,6 +12,8 @@ import java.io.PrintStream;
  */
 public class PaFacesGenerator {
 
+    private static final String HEADER="import java.util.List;\nimport java.io.PrintStream;\nimport java.util.Calendar;\n";
+
     private Code code;
 
     public PaFacesGenerator() {
@@ -21,13 +23,14 @@ public class PaFacesGenerator {
     public void writeCode(PrintStream pout,PaFacesObject root){
         root.getCode(code);
         pout.println(code.head);
+        pout.println(HEADER);
         pout.println("public class Page implements WebComponent {\n");
         pout.println(code.var);
         pout.println("\tpublic Page() {");
         pout.print(code.constr);
         pout.println("\t}\n\tpublic void preRender(List<String> headtext){");
         pout.print(code.preRender);
-        pout.println("\t}\n\tpublic void render(PrintStream output, List<String> headtext){");
+        pout.println("\t}\n\tpublic void render(PrintStream output, List<String> headText){");
         pout.print(code.render);
         pout.println("\t}");
         pout.println("}");
