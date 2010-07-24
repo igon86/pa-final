de992028222f8a26c1e6ed330009791f5d38b1a6
andre.lotta86 <andre.lotta86@4c4e6b51-19cc-038f-6026-35b58781e471>
2010-07-24 13:49:14 +0000

    aggiustata libreria e messo micro css nella Label

--- trunk/WebComponent/src/mylib/ComponentPair.java
+++ trunk/WebComponent/src/mylib/ComponentPair.java
@@ -4,15 +4,6 @@ package mylib;
 import java.io.PrintStream;
 import java.util.List;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author andrealottarini
- */
 public class ComponentPair implements WebComponent {
     public WebComponent first;
     public WebComponent second;
@@ -21,9 +12,9 @@ public class ComponentPair implements WebComponent {
         
     }
 
-    public void render(PrintStream output, List<String> headText) {
-        first.render(output, headText);
-        second.render(output, headText);
+    public void render(PrintStream output) {
+        first.render(output);
+        second.render(output);
     }
 
 }
--- trunk/WebComponent/src/mylib/WebComponent.java
+++ trunk/WebComponent/src/mylib/WebComponent.java
@@ -1,20 +1,9 @@
 package mylib;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 import java.io.PrintStream;
 import java.util.List;
 
-/**
- *
- * @author andrealottarini
- */
 public interface WebComponent {
     void preRender(List<String> headtext);
-    void render(PrintStream output,List<String> headText);
+    void render(PrintStream output);
 }
