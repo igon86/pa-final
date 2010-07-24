cd94ecd79ee714ee07f6e4e31c2ab6fd639dce17
andre.lotta86 <andre.lotta86@4c4e6b51-19cc-038f-6026-35b58781e471>
2010-07-24 13:49:14 +0000

    aggiustata libreria e messo micro css nella Label

--- WebComponent/src/mylib/Calendario.java
+++ WebComponent/src/mylib/Calendario.java
@@ -1,26 +1,13 @@
 package mylib;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 import java.io.PrintStream;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Locale;
 
-/**
- *
- * @author andrealottarini
- */
-public class Calendario implements WebComponent{
-    
+public class Calendario implements WebComponent {
+
     public Calendar selectedDay;
-
     private static final String header = "<td valign=\"top\">\n" +
             "<table bordercolordark=\"#000000\" bordercolorlight=\"#FFFFFF\" border=\"1\" cellpadding=\"5\" width=\"100%\" <tbody><tr>\n" +
             "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Sun </font></th>" +
@@ -29,55 +16,45 @@ public class Calendario implements WebComponent{
             "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Wed </font></th>" +
             "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Thu </font></th>" +
             "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Fri </font></th>" +
-            "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Sat </font></th>" +           
+            "<th align=\"center\" bgcolor=\"#000000\" valign=\"center\" width=\"14%\"><font color=\"#ffffff\" face=\"Verdana\" size=\"1\"> Sat </font></th>" +
             "</tr>";
-
-    private static final String table_entry="<td align=\"left\" valign=\"top\" width=\"14%\">";
-
-    private static final String table_closure="</td>";
-
-    private static final int DAY_OF_THE_WEEK=7;
-
+    private static final String table_entry = "<td align=\"left\" valign=\"top\" width=\"14%\">";
+    private static final String table_closure = "</td>";
+    private static final int DAY_OF_THE_WEEK = 7;
     private static final String footer = "</tr></tbody></table> </td> </tr> ";
 
-    public Calendario(Calendar c){
+    public Calendario(Calendar c) {
         this.selectedDay = c;
     }
 
-    public Calendario(){
-        
+    public Calendario() {
     }
 
-    @Override
     public void preRender(List<String> headtext) {
-        
     }
 
-    @Override
-    public void render(PrintStream output,List<String> headtext) {
-        System.out.println("ANNO: "+selectedDay.get(Calendar.YEAR)+" MESE: "+selectedDay.get(Calendar.MONTH));
-        System.out.println("GIORNO DELLA SETTIMANA: "+selectedDay.get(Calendar.DAY_OF_WEEK));
-        Calendar first = new GregorianCalendar(selectedDay.get(Calendar.YEAR),selectedDay.get(Calendar.MONTH),1);
-        System.out.println("ANNO: "+first.get(Calendar.YEAR)+" MESE: "+first.get(Calendar.MONTH));
-        System.out.println("GIORNO DELLA SETTIMANA: "+first.get(Calendar.DAY_OF_WEEK));
-//        System.out.println("First day of week: "+first.getFirstDayOfWeek());
-//        first.setFirstDayOfWeek(Calendar.WEDNESDAY);
-//        System.out.println("First day of week: "+first.getFirstDayOfWeek());
+    public void render(PrintStream output) {
+
+        Calendar first = new GregorianCalendar(selectedDay.get(Calendar.YEAR), selectedDay.get(Calendar.MONTH), 1);
+
+
         int offset = first.get(Calendar.DAY_OF_WEEK);
-        System.out.println("offset: "+offset);
+
         int days = first.getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.println("Ci sono " +days +" giorni.");
+
         output.println(header);
-        int i=1;
-        for(;i<offset + days;i++){
-            if((i-1)%DAY_OF_THE_WEEK == 0){
+        int i = 1;
+        for (; i < offset + days; i++) {
+            if ((i - 1) % DAY_OF_THE_WEEK == 0) {
                 output.println("</tr><tr>");
             }
             output.print(table_entry);
-            if (i>=offset) output.print(i-offset+1);
+            if (i >= offset) {
+                output.print(i - offset + 1);
+            }
             output.println(table_closure);
         }
-        while((i-1)%DAY_OF_THE_WEEK != 0){
+        while ((i - 1) % DAY_OF_THE_WEEK != 0) {
             output.println(table_entry + table_closure);
             i++;
         }
--- WebComponent/src/mylib/Label.java
+++ WebComponent/src/mylib/Label.java
@@ -1,19 +1,8 @@
 package mylib;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 import java.io.PrintStream;
 import java.util.List;
 
-/**
- *
- * @author andrealottarini
- */
 public class Label implements WebComponent{
     public String text;
 
@@ -25,14 +14,12 @@ public class Label implements WebComponent{
         
     }
 
-    @Override
     public void preRender(List<String> headtext) {
-        
+        headtext.add("<style type=\"text/css\">\nspan.Label {color:red;}</style>");
     }
 
-    @Override
-    public void render(PrintStream output,List<String> headtext) {
-        output.println("<span style=\"font-weight : bold;\">"+text+"</span>");
+    public void render(PrintStream output) {
+        output.println("<span class=\"Label\"\">"+text+"</span>");
     }
 
 
